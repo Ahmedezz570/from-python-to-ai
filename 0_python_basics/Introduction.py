3a0a9982411bccfe8dd922b7d4734c567ab2ef9d
Ahmedezz570 <ahmedez570@gmail.com>
2025-11-08 23:07:22 +0200

    add Introduction and Variable file

---
+++ 0_python_basics/Introduction.py
@@ -0,0 +1,17 @@
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+
+# Semicolons (Optional, Rarely Used)
+print("Hello"); print("How are you?"); print("Bye bye!")
+
+# Print Numbers
+print(3)
+print(358)
+print(50000)
+# You can also do math inside the print() function:
+print(3 + 3)
+print(2 * 5)
+# Mix Text and Numbers
+print("I am", 35, "years old.")
--- 0_python_basics/Variables.py
+++ 0_python_basics/Variables.py
@@ -1,3 +1,53 @@
-print("Variable 1")
-print("Variable 2")
-print("Variable 3")
+# Creating Variables
+# Python has no command for declaring a variable.
+# A variable is created the moment you first assign a value to it.
+x = 5  # int
+y = "John" # string(str)
+print(x)
+print(y)
+# Casting
+a = float(3000)
+b = str(200)
+print(type(a))
+print(type(b))
+# Single or Double Quotes?
+x = "John"
+# is the same as
+x = 'John'
+# Case-Sensitive
+a = 4
+A = "Sally"
+#A will not overwrite a
+# Variable Names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+# Camel Case
+myVariableName = "John"
+# Pascal Case
+MyVariableName = "John"
+# Snake Case
+my_variable_name = "John"
+# Many Values to Multiple Variables
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+# One Value to Multiple Variables
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+# Notice the space character after "Python " and "is ", without them the result would be "Pythonisawesome".
+
